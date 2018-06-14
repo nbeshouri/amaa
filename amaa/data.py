a0447b37d1523165f736062f65941fc3ccaa99f8
nbeshouri <nbeshouri@gmail.com>
2018-06-14 13:32:24 -0700

    Add data.py

---
+++ amaa/data.py
@@ -0,0 +1,314 @@
+"""
+This module contains a collection of functions used to generate different
+versions of the data set.
+
+Todo:
+    
+
+"""
+
+
+
+# from . import utils
+import os
+from glob import glob
+import re
+import numpy as np
+from munch import Munch
+from itertools import chain
+from . import transforms
+from . import utils
+
+
+data_folder_path = os.path.join(os.path.dirname(__file__), 'data')
+_babi_1K_path = os.path.join(data_folder_path, 'tasks_1-20_v1-2', 'en')
+_babi_10K_path = os.path.join(data_folder_path, 'tasks_1-20_v1-2', 'en-10k')
+data_dir_path = os.path.join(os.path.dirname(__file__), 'data')
+
+
+def _read_babi_lines(path):
+    
+    story_lines = []
+
+    with open(path) as f:
+        for line in f:
+            line_num = int(line.split(' ')[0])
+            if line_num == 1 and story_lines:
+                story_lines = []
+            if '?' in line:
+                yield story_lines, line
+            else:
+                story_lines.append(line)
+
+
+def load_task(task_num, version, folder_path):
+    """
+    Creates a set of QA tuples for a given task.
+    """
+    glob_path = os.path.join(folder_path, f'qa{task_num}_*{version}*')
+    task_path = glob(glob_path)[0]
+    
+    output = []
+    
+    for story_lines, question_line in _read_babi_lines(task_path):
+        story = ''.join(story_lines)
+        story = story.replace('\n', ' ')
+        story = re.sub(r'\d+\s', r'', story)
+        question, answer, _ = question_line.split('\t')
+        question = re.sub(r'\d+\s', r'', question)
+        output.append((story, question, answer))
+    
+    return output
+
+
+def load_tasks(version, folder_path):
+    """
+    Load the QA tuples for all tasks.
+    """
+    output = []
+    for i in range(1, 21):
+        output.extend(load_task(i, version, folder_path))
+    return output
+
+
+def get_embeddings(texts, min_vocab_size=0):
+    """
+    Build emeddding mappings based on `texts`.
+
+    Args:
+        texts (Iterable[str]): A sequence of text to fit the embeddings
+            on.
+        min_vocab_size (Optional[int]): Minimum number of words, not
+            counting those in `texts` to include in the embedding
+            vocabulary.
+
+    Returns:
+        (tuple): tuple containing:
+            word_to_vec (dict): A map between word tokens and numpy vectors.
+            word_to_id (dict): A map between word tokens and embedding ids.
+            embedding_matrix (np.ndarry): A numpy matrix that maps between
+                embedding ids and embedding vectors.
+
+    """
+    data_vocab = set()
+    for token_list in transforms.token_strs_to_token_lists(texts):
+        for token in token_list:
+            data_vocab.add(token)
+
+    word_to_vec = {}
+    embeddings_path = os.path.join(data_dir_path, 'glove.6B/glove.6B.200d.txt')
+    with open(embeddings_path) as f:
+        for line_num, line in enumerate(f):
+            values = line.split()
+            word = values[0]
+            if min_vocab_size < line_num + 1 and word not in data_vocab:
+                continue
+            vector = np.asarray(values[1:], dtype='float32')
+            word_to_vec[word] = vector
+
+    total_vocab = data_vocab | set(word_to_vec.keys())
+    rand_state = np.random.RandomState(42)
+    word_to_id = {'<PAD>': 0, '<EOS>': 1, '<START>': 2}
+    num_meta_tokens = 3
+    embedding_matrix = rand_state.rand(len(total_vocab) + num_meta_tokens, 200)
+    embedding_matrix[0] = 0
+    embedding_matrix[1] = 1
+    embedding_matrix[2] = 2
+    for i, word in enumerate(total_vocab):
+        word_id = i + num_meta_tokens
+        if word in word_to_vec:
+            embedding_matrix[word_id] = word_to_vec[word]
+        word_to_id[word] = word_id
+
+    return word_to_vec, word_to_id, embedding_matrix
+
+
+def ids_to_text(ids, id_to_word):
+    tokens = [id_to_word[id] for id in ids]
+    return ' '.join(tokens)
+
+    
+def id_lists_to_texts(id_lists, id_to_word):
+    return [ids_to_text(ids, id_to_word) for ids in id_lists]
+
+
+def get_train_val_test(train_sqas, test_sqas):
+    
+    def process_sqas(sqas):
+        token_sqas = map(transforms.tokenize_texts, zip(*sqas))
+        token_sqas = tuple(map(np.array, token_sqas))
+        return token_sqas
+    
+    # train_stories, train_questions, train_answers = tuple(map(np.array, zip(*train_sqas)))
+    # test_stories, test_questions, test_answers = tuple(map(np.array, zip(*test_sqas))) 
+    
+    train_stories, train_questions, train_answers = process_sqas(train_sqas)
+    test_stories, test_questions, test_answers = process_sqas(test_sqas)
+    
+    _, val_indices, test_indices = get_train_val_test_indices(len(test_stories), val_ratio=0.50, test_ratio=0.50)
+    
+    data = Munch(
+        X_train_stories=train_stories,
+        X_train_questions=train_questions,
+        y_train=train_answers,
+        
+        X_val_stories=test_stories[val_indices],
+        X_val_questions=test_questions[val_indices],
+        y_val=test_answers[val_indices],
+        
+        X_test_stories=test_stories[test_indices],
+        X_test_questions=test_questions[test_indices],
+        y_test=test_answers[test_indices]
+    )
+    
+    return data
+
+
+def texts_to_ids(texts, word_to_id, max_sequence_length=None):
+    """
+    Args:
+        texts (Iterable[str]): A sequence of texts to fit the embeddings
+            on.
+        word_to_id (dict): A map between word tokens and embedding ids.
+        max_sequence_length (Optional[int]): The maximum number of words
+            to include in each line of dialogue. Shorter sequences will
+            be padded with the <PAD> vector.
+
+    Returns:
+        X (np.ndarray): An array with shape `(len(texts), max_sequence_length)`
+            containing the correct embedding ids.
+
+    """
+    
+    token_lists = transforms.token_strs_to_token_lists(texts)
+    texts_ids = []
+    for token_list in token_lists:
+        word_ids = []
+        for token in token_list:
+            if token in word_to_id:
+                word_ids.append(word_to_id[token])
+        # Add <EOS> token.
+        word_ids.append(1)  
+        texts_ids.append(word_ids)
+    
+    if max_sequence_length is None:
+        max_sequence_length = max(len(ids) for ids in texts_ids)
+    
+    X = np.zeros((len(texts), max_sequence_length), dtype=int)
+    for i, text_ids in enumerate(texts_ids):
+        text_ids = text_ids[:max_sequence_length]
+        X[i, :len(text_ids)] = text_ids
+
+    return X
+
+
+def align_padding(*id_matrices):
+    max_row_len = max(matrix.shape[1] for matrix in id_matrices)
+    output = []
+    for matrix in id_matrices:
+        padding_needed = max_row_len - matrix.shape[1]
+        padded_matrix = np.pad(matrix, ((0, 0), (0, padding_needed)), 
+                               'constant', constant_values=(0, 0))
+        output.append(padded_matrix)
+    return output
+
+
+def add_nn_X_y(data, word_to_id, X_padding):
+    # This should be smart enough to add the correct amount of
+    # padding on its own. 
+    keys = [key for key in data.keys if 'X_' in key]
+    for key in keys:
+        data[key] = to_ids(data[key])
+    return data
+    
+    
+    # data = Munch(
+    #     X_train_stories=train_stories,
+    #     X_train_questions=train_questions,
+    #     y_train=train_answers,
+    # 
+    #     X_val_stories=test_stories[val_indices],
+    #     X_val_questions=test_questions[val_indices],
+    #     y_val=test_answers[val_indices],
+    # 
+    #     X_test_stories=test_stories[test_indices],
+    #     X_test_questions=test_questions[test_indices],
+    #     y_test=test_answers[test_indices]
+    # )
+
+
+def get_train_val_test_indices(num_rows, val_ratio=0.25, test_ratio=0.25):
+    """Return indices of the train, test, and validation sets."""
+    rand = np.random.RandomState(42)
+    indices = rand.permutation(range(num_rows))
+    train_ratio = 1 - val_ratio - test_ratio
+    train_indices = indices[:int(len(indices) * train_ratio)]
+    val_indices = indices[len(train_indices):len(train_indices) + int(len(indices) * val_ratio)]
+    test_indices = indices[len(train_indices) + len(val_indices):]
+    return train_indices, val_indices, test_indices
+    
+
+def get_babi_data(use_10k=False):
+    babi_path = _babi_10K_path if use_10k else _babi_1K_path
+    train_tasks = load_tasks('train', babi_path)
+    test_tasks = load_tasks('test', babi_path)
+    data = get_train_val_test(train_tasks, test_tasks)
+
+    data.word_to_vec, data.word_to_id, data.embedding_matrix = get_embeddings(chain(*data.values()))
+    data.id_to_word = {id: word for word, id in data.word_to_id.items()}
+    
+    keys = [key for key in data.keys() if 'X_' in key or 'y_' in key]
+    for key in keys:
+        data[key] = texts_to_ids(data[key], data.word_to_id)
+    
+    data.X_train_stories, data.X_val_stories, data.X_test_stories = align_padding(
+        data.X_train_stories, data.X_val_stories, data.X_test_stories)
+    data.X_train_questions, data.X_val_questions, data.X_test_questions = align_padding(
+        data.X_train_questions, data.X_val_questions, data.X_test_questions)
+    data.y_train, data.y_val, data.y_test = align_padding(
+        data.y_train, data.y_val, data.y_test)
+    
+    return data
+    
+small_babi = get_babi_data()
+utils.save_data(small_babi, 'babi_1K_data.pickle')
+
+print('Done small.')
+
+big_babi = get_babi_data(use_10k=True)
+utils.save_data(big_babi, 'babi_10k_data.pickle')
+
+# TODO: Decorator that takes a cache name and will cache the output for a given 
+# set of args. It should also take an optional flag to recompute data. Or it could
+# hash the source code file to see if anything has changed. 
+    
+
+get_babi_data()
+    
+# train_tasks = load_tasks('train', _babi_1K_path)[:100]
+# test_tasks = load_tasks('test', _babi_1K_path)[:100]
+# 
+# # train_tasks = load_task(8, 'train', _babi_1K_path)
+# # test_tasks = load_task(8, 'test', _babi_1K_path)
+# data = get_train_val_test(train_tasks, test_tasks)
+# 
+# print(data.X_val_stories[3])
+# print(data.X_val_questions[3])
+# print(data.y_val[3])
+# 
+# print(data.X_train_stories[54])
+# print(data.X_train_questions[54])
+# print(data.y_train[54])
+
+
+
+# print(len(train_tasks))
+# print(train_tasks[:100])
+# get_train_val_test(train_tasks, test_tasks)
+
+
+# print(len(output))
+
+    
+
+

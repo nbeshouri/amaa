388f73d275020e15b6c4fd0fbcbe2820f40f5d2c
nbeshouri <nbeshouri@gmail.com>
2018-11-04 19:11:27 -0800

    Fix bugs in PyTorch version

--- amaa/pytorch_models.py
+++ amaa/pytorch_models.py
@@ -1,7 +1,10 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from itertools import chain
+import time
+from sklearn.metrics import accuracy_score
 
 
 class EpisodicGRU(nn.Module):
@@ -98,7 +101,8 @@ class DMNEncoder(nn.Module):
         self.encoder = nn.GRU(
             input_size=embedding_size,
             hidden_size=recur_size,
-            num_layers=1)
+            num_layers=1,
+            batch_first=True)
 
         self.gate_linear = nn.Linear(
             in_features=recur_size * 4,
@@ -186,14 +190,15 @@ class DMNDecoder(nn.Module):
         self.decoder = nn.GRU(
             input_size=recur_size + embedding_size,
             hidden_size=recur_size,
-            num_layers=1)
+            num_layers=1,
+            batch_first=True)
 
         self.word_predictor = nn.Linear(
             recur_size,
             vocab_size)
 
 
-    def forward(self, x, question_vecs, memory_encoder_state):
+    def forward(self, prev_words, question_vecs, memory_encoder_state):
         """
 
         Args:
@@ -203,20 +208,112 @@ class DMNDecoder(nn.Module):
         Returns:
 
         """
-        x = self.embeddings(x)
+        prev_words = self.embeddings(prev_words)
         question_vecs = question_vecs.unsqueeze(1)
-        repeated_question_vecs = question_vecs.expand(-1, x.size(1), -1)
-        decoder_inputs = torch.cat([x, repeated_question_vecs], dim=2)
+        repeated_question_vecs = question_vecs.expand(-1, prev_words.size(1), -1)
+        decoder_inputs = torch.cat([prev_words, repeated_question_vecs], dim=2)
         decoder_outputs, state = self.decoder(decoder_inputs, memory_encoder_state)
         predicted_words = self.word_predictor(decoder_outputs)
         return predicted_words, state
 
 
+class BabAIDataset(Dataset):
+
+    def __init__(
+            self, X_stories, X_story_masks,
+            X_questions, y, X_decoder=None, X_hints=None):
+        self.X_stories = torch.LongTensor(X_stories)
+        self.X_questions = torch.LongTensor(X_questions)
+        self.X_story_masks = torch.ByteTensor(X_story_masks)
+        self.X_decoder = torch.LongTensor(X_decoder)
+        self.X_hints = X_hints
+        self.y = y
+
+    def __len__(self):
+        return len(self.X_stories)
+
+    def __getitem__(self, item):
+        batch = [self.X_stories[item], self.X_story_masks[item], self.X_questions[item], self.y[item]]
+        if self.X_decoder is not None:
+            batch.append(self.X_decoder[item])
+        if self.X_hints is not None:
+            batch.append(self.X_hints[item])
+        return batch
 
 
 def predict(encoder_model, decoder_model, stories, questions):
     pass
 
-def train(encoder_model, decoder_model, data, epochs):
-    pass
+
+def train(encoder_model, decoder_model, data, epochs, log_freq=5):
+
+    train_dataset = BabAIDataset(
+        data.X_train_stories,
+        data.X_train_story_masks,
+        data.X_train_questions,
+        data.y_train,
+        data.X_train_decoder,
+        data.X_train_hints)
+
+    train_dataloader = DataLoader(train_dataset, batch_size=64)
+
+    val_dataset = BabAIDataset(
+        data.X_val_stories,
+        data.X_val_story_masks,
+        data.X_val_questions,
+        data.y_val,
+        data.X_val_decoder,
+        data.X_val_hints)
+
+    val_dataloader = DataLoader(val_dataset)
+
+    criterion = nn.CrossEntropyLoss()
+    # The embedding layer is in both and is trainable, so make it's not
+    # fed to the optimizer more than once.
+    params = set(chain(encoder_model.parameters(), decoder_model.parameters()))
+    optimizer = torch.optim.RMSprop(params)
+
+    for epoch in range(epochs):
+        print(f'Starting epoch {epoch}.')
+        epoch_start_time = time.time()
+        running_loss = 0.0
+        running_accuracy = 0.0
+        encoder_model.train()
+        decoder_model.train()
+        for i, train_batch in enumerate(train_dataloader):
+            X_stories, X_story_masks, X_questions, y, X_decoder, X_hints = train_batch
+
+            optimizer.zero_grad()
+            question_vecs, encoder_state = encoder_model(X_stories, X_story_masks, X_questions)
+            y_pred, _ = decoder_model(X_decoder, question_vecs, encoder_state)
+            y_pred = y_pred.reshape(-1, y_pred.size(2))
+            y = y.reshape(-1)
+            loss = criterion(y_pred, y)
+            accuracy = accuracy_score(y, y_pred.argmax(dim=-1))
+            loss.backward()
+            optimizer.step()
+
+            # Periodically print dthe loss and prediction accuracy. Usually
+            # with a language model you'd also show perplexity, but as it's
+            # a function of our cross entropy loss and not that intuitive,
+            # I've elected not to.
+            running_loss += loss.item()
+            running_accuracy += accuracy
+            if i % log_freq == log_freq - 1:
+                average_loss = running_loss / log_freq
+                average_accuracy = running_accuracy / log_freq
+                print(f'Mini-batch: {i + 1}/{len(train_dataloader)} '
+                      f'Loss: {average_loss:.5f} Accuracy: {average_accuracy:.5f}')
+                running_loss = 0.0
+                running_accuracy = 0.0
+
+    # Log elapsed_time for the epoch.
+    elapsed_time = time.time() - epoch_start_time
+    print(f'Epoch {epoch} completed in {elapsed_time // 60:.0f} minutes '
+          f'{elapsed_time % 60:.0f} seconds.\n')
+
+
+
+
+
 

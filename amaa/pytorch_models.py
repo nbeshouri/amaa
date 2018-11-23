d7768a15c9e506d764d156fdd5bc5b80a4aa0ca3
nbeshouri <nbeshouri@gmail.com>
2018-11-22 20:13:48 -0800

    Add gate supervision option

--- amaa/pytorch_models.py
+++ amaa/pytorch_models.py
@@ -4,6 +4,7 @@ import numpy as np
 from torch.utils.data import Dataset, DataLoader
 from itertools import chain
 import time
+from . import data
 from sklearn.metrics import accuracy_score
 
 
@@ -85,7 +86,7 @@ class EpisodicGRU(nn.Module):
 class DMNEncoder(nn.Module):
 
     def __init__(
-            self, recur_size=256, embedding_matrix=None,
+            self, recur_size=128, embedding_matrix=None,
             train_embeddings=False, iterations=3):
         super().__init__()
         self.recur_size = recur_size
@@ -134,6 +135,7 @@ class DMNEncoder(nn.Module):
 
         # Encode stories.
         x = self.embeddings(x_stories)
+        # TODO: Can't this one be done with packed sequences too?
         encoded_stories, _ = self.encoder(x)
         sent_counts = np.sum(np.array(x_story_masks), axis=1)  # (batch_size,)
         masked_outputs = encoded_stories[x_story_masks]  # (num_sents_in_batch, recur_size)
@@ -147,8 +149,8 @@ class DMNEncoder(nn.Module):
         # `masked_outputs` is a flat tensor of encoded sentences that
         # have been selected with boolean masking. Here we unravel those
         # a `(batch_size, max_num_sents, recur_size)` tensor for encoding
-        # into episode vectors. They can't just be reshaped because the 
-        # size of the array is changing.
+        # into episode vectors. They can't just be reshaped because the
+        # size of the array is changing (stories are of different lengths).
         masked_sent_index = 0
         for story_id, sent_count in enumerate(sent_counts):
             for sent_id in range(sent_count):
@@ -180,17 +182,17 @@ class DMNEncoder(nn.Module):
             # be selecting the value at the last timestep with input. Or
             # I could them into a packed sequence...
 
-            # TODO: Here, I'm totally ignoring the separate memory 
+            # TODO: Here, I'm totally ignoring the separate memory
             # encoder. It should probably be optional.
             memory_vecs = episode_vecs.unsqueeze(1)  # (batch_size, 1, recur_size)
             mem_encoder_state = ep_encoder_state
 
-        return questions_vecs, mem_encoder_state
+        return questions_vecs, mem_encoder_state, gate_weights
 
 
 class DMNDecoder(nn.Module):
 
-    def __init__(self, embeddings, recur_size=256):
+    def __init__(self, embeddings, recur_size=128):
         super().__init__()
         vocab_size = embeddings.num_embeddings
         embedding_size = embeddings.weight.size(1)
@@ -230,31 +232,72 @@ class BabAIDataset(Dataset):
 
     def __init__(
             self, X_stories, X_story_masks,
-            X_questions, y, X_decoder=None, X_hints=None):
+            X_questions, y, X_decoder=None, y_att=None):
         self.X_stories = torch.LongTensor(X_stories)
         self.X_questions = torch.LongTensor(X_questions)
         self.X_story_masks = torch.ByteTensor(X_story_masks)
         self.X_decoder = torch.LongTensor(X_decoder)
-        self.X_hints = X_hints
+        if y_att is not None:
+            y_att = torch.FloatTensor(self.get_att_target(y_att, X_story_masks))
+        self.y_att = y_att
         self.y = y
 
     def __len__(self):
         return len(self.X_stories)
 
+    @staticmethod
+    def get_att_target(sent_indices, story_masks):
+        """
+        Convert attention targets from lists of sentence indices to
+        boolean masks.
+
+        TODO: This really should be part of the data structure that
+        defines the data, but the existing version of the masks was
+        written assuming pre-padding.
+        """
+        sent_counts = np.sum(np.array(story_masks), axis=1, dtype=int)
+        att = np.zeros((story_masks.shape[0], max(sent_counts)))
+        for example_index, indicies in enumerate(sent_indices):
+            for sent_index in indicies:
+                att[example_index, sent_index] = 1
+        return att
+
     def __getitem__(self, item):
         batch = [self.X_stories[item], self.X_story_masks[item], self.X_questions[item], self.y[item]]
         if self.X_decoder is not None:
             batch.append(self.X_decoder[item])
-        if self.X_hints is not None:
-            batch.append(self.X_hints[item])
+        if self.y_att is not None:
+            batch.append(self.y_att[item])
         return batch
+
+
+# def debug(data_munch):
+#     index = 0
+#     example_story = data_munch.X_train_stories[index]
+#     example_question = data_munch.X_train_questions[index]
+#     example_hint = data_munch.y_train_att[index]
+#     print(data.ids_to_text(example_story, data_munch.id_to_word))
+#     print(data.ids_to_text(example_question, data_munch.id_to_word))
+#     print(example_hint)
+#     print(data_munch.train_hints[index])
+#     print(data_munch.X_train_story_masks[index])
+
+
+def debug(data_munch, dataset, id_to_word):
+    index = 2
+    example_story = dataset.X_stories[index]
+    example_question = dataset.X_questions[index]
+    print(data.ids_to_text(np.array(example_story), id_to_word))
+    print(data.ids_to_text(np.array(example_question), id_to_word))
+    print(dataset.y_att[index])
+    print(data_munch.train_hints[index])
 
 
 def predict(encoder_model, decoder_model, stories, questions):
     pass
 
 
-def train(encoder_model, decoder_model, data, epochs, log_freq=5):
+def train(encoder_model, decoder_model, data, epochs, log_freq=1, gate_supervision=True):
 
     train_dataset = BabAIDataset(
         data.X_train_stories,
@@ -262,7 +305,7 @@ def train(encoder_model, decoder_model, data, epochs, log_freq=5):
         data.X_train_questions,
         data.y_train,
         data.X_train_decoder,
-        data.X_train_hints)
+        data.train_hints)
 
     train_dataloader = DataLoader(train_dataset, batch_size=64)
 
@@ -272,11 +315,16 @@ def train(encoder_model, decoder_model, data, epochs, log_freq=5):
         data.X_val_questions,
         data.y_val,
         data.X_val_decoder,
-        data.X_val_hints)
+        data.val_hints)
 
     val_dataloader = DataLoader(val_dataset)
 
+    # debug(data, train_dataset, data.id_to_word)
+    # return
+
     criterion = nn.CrossEntropyLoss()
+    att_criterion = nn.BCELoss()
+
     # The embedding layer is in both and is trainable, so make it's not
     # fed to the optimizer more than once.
     params = set(chain(encoder_model.parameters(), decoder_model.parameters()))
@@ -290,29 +338,47 @@ def train(encoder_model, decoder_model, data, epochs, log_freq=5):
         encoder_model.train()
         decoder_model.train()
         for i, train_batch in enumerate(train_dataloader):
-            X_stories, X_story_masks, X_questions, y, X_decoder, X_hints = train_batch
+            X_stories, X_story_masks, X_questions, y, X_decoder, y_att = train_batch
 
             optimizer.zero_grad()
-            question_vecs, encoder_state = encoder_model(X_stories, X_story_masks, X_questions)
+            question_vecs, encoder_state, att_pred = encoder_model(X_stories, X_story_masks, X_questions)
             y_pred, _ = decoder_model(X_decoder, question_vecs, encoder_state)
             y_pred = y_pred.reshape(-1, y_pred.size(2))
             y = y.reshape(-1)
-            loss = criterion(y_pred, y)
+            decoder_loss = criterion(y_pred, y)
+
+            # TODO: The next step is going to split the two losses so that you
+            # can see the differences.
+
+            total_loss = decoder_loss
+            if gate_supervision:
+                trunc_y_att = y_att[:, :att_pred.size(1)]
+                att_loss = att_criterion(att_pred, trunc_y_att)
+                # total_loss += att_loss
+                total_loss = total_loss + att_loss
+
             accuracy = accuracy_score(y, y_pred.argmax(dim=-1))
-            loss.backward()
+            total_loss.backward()
             optimizer.step()
 
             # Periodically print dthe loss and prediction accuracy. Usually
             # with a language model you'd also show perplexity, but as it's
             # a function of our cross entropy loss and not that intuitive,
             # I've elected not to.
-            running_loss += loss.item()
+            running_loss += decoder_loss.item()
             running_accuracy += accuracy
-            if i % log_freq == log_freq - 1:
+            # if i % log_freq == log_freq - 1:
+            # print((i + 1) % log_freq)
+            if (i + 1) % log_freq == 0:
                 average_loss = running_loss / log_freq
                 average_accuracy = running_accuracy / log_freq
-                print(f'Mini-batch: {i + 1}/{len(train_dataloader)} '
-                      f'Loss: {average_loss:.5f} Accuracy: {average_accuracy:.5f}')
+                log_str = f'Mini-batch: {i + 1}/{len(train_dataloader)} '
+                if gate_supervision:
+                    log_str += (f'Decoder loss: {decoder_loss:.5f} Attention Loss: {att_loss:.5f} '
+                                f'Total loss: {total_loss:.5f} Accuracy: {average_accuracy:.5f}')
+                else:
+                    log_str += f'Loss: {average_loss:.5f} Accuracy: {average_accuracy:.5f}'
+                print(log_str)
                 running_loss = 0.0
                 running_accuracy = 0.0
 
@@ -320,9 +386,3 @@ def train(encoder_model, decoder_model, data, epochs, log_freq=5):
     elapsed_time = time.time() - epoch_start_time
     print(f'Epoch {epoch} completed in {elapsed_time // 60:.0f} minutes '
           f'{elapsed_time % 60:.0f} seconds.\n')
-
-
-
-
-
-
